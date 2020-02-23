4d3c076c2ac0be4d516f0bc9f40df035ebaaff9c
Heng Li <lh3@me.com>
2020-02-22 22:49:38 -0500

    added kc-c3

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CFLAGS=-g -Wall -O2
 LIBS=-lz
-PROG=kc-c1 kc-c2
+PROG=kc-c1 kc-c2 kc-c3
 
 ifneq ($(asan),)
 	CFLAGS+=-fsanitize=address
@@ -16,6 +16,9 @@ kc-c1:kc-c1.c khashl.h ketopt.h kseq.h
 
 kc-c2:kc-c2.c khashl.h ketopt.h kseq.h
 	$(CC) $(CFLAGS) -o $@ $< $(LIBS)
+
+kc-c3:kc-c3.c khashl.h ketopt.h kseq.h kthread.h
+	$(CC) $(CFLAGS) -o $@ kc-c3.c kthread.c $(LIBS) -lpthread
 
 clean:
 	rm -fr *.dSYM $(PROG)
--- kc-c3.c
+++ kc-c3.c
@@ -2,16 +2,18 @@
 #include <stdint.h>
 #include <zlib.h>
 #include "ketopt.h"
+#include "kthread.h"
 
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
 
 #include "khashl.h"
-#define kc_c2_eq(a, b) ((a)>>8 == (b)>>8)
-#define kc_c2_hash(a) ((a)>>8)
-KHASHL_SET_INIT(, kc_c2_t, kc_c2, uint64_t, kc_c2_hash, kc_c2_eq)
+#define kc_c3_eq(a, b) ((a)>>8 == (b)>>8)
+#define kc_c3_hash(a) ((a)>>8)
+KHASHL_SET_INIT(, kc_c3_t, kc_c3, uint64_t, kc_c3_hash, kc_c3_eq)
 
 #define CALLOC(ptr, len) ((ptr) = (__typeof__(ptr))calloc((len), sizeof(*(ptr))))
+#define REALLOC(ptr, len) ((ptr) = (__typeof__(ptr))realloc((ptr), (len) * sizeof(*(ptr))))
 
 const unsigned char seq_nt4_table[256] = {
 	0, 1, 2, 3,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
@@ -46,31 +48,31 @@ static inline uint64_t hash64(uint64_t key, uint64_t mask)
 
 typedef struct {
 	int p;
-	kc_c2_t **h;
-} kc_c2x_t;
+	kc_c3_t **h;
+} kc_c3x_t;
 
-static kc_c2x_t *c2x_init(int p)
+static kc_c3x_t *c3x_init(int p)
 {
 	int i;
-	kc_c2x_t *h;
+	kc_c3x_t *h;
 	CALLOC(h, 1);
 	CALLOC(h->h, 1<<p);
 	h->p = p;
 	for (i = 0; i < 1<<p; ++i)
-		h->h[i] = kc_c2_init();
+		h->h[i] = kc_c3_init();
 	return h;
 }
 
-static inline void c2x_insert(kc_c2x_t *h, uint64_t y)
+static inline void c3x_insert(kc_c3x_t *h, uint64_t y)
 {
 	int absent, pre = y & ((1<<h->p) - 1);
-	kc_c2_t *g = h->h[pre];
+	kc_c3_t *g = h->h[pre];
 	khint_t k;
-	k = kc_c2_put(g, y>>h->p<<8, &absent);
+	k = kc_c3_put(g, y>>h->p<<8, &absent);
 	if ((kh_key(g, k)&0xff) < 255) ++kh_key(g, k);
 }
 
-static void count_seq(kc_c2x_t *h, int k, int len, char *seq)
+static void count_seq(kc_c3x_t *h, int k, int len, char *seq)
 {
 	int i, l;
 	uint64_t x[2], mask = (1ULL<<k*2) - 1, shift = (k - 1) * 2;
@@ -81,35 +83,82 @@ static void count_seq(kc_c2x_t *h, int k, int len, char *seq)
 			x[1] = x[1] >> 2 | (uint64_t)(3 - c) << shift;  // reverse strand
 			if (++l >= k) { // we find a k-mer
 				uint64_t y = x[0] < x[1]? x[0] : x[1];
-				c2x_insert(h, hash64(y, mask));
+				c3x_insert(h, hash64(y, mask));
 			}
 		} else l = 0, x[0] = x[1] = 0; // if there is an "N", restart
 	}
 }
 
-static kc_c2x_t *count_file(const char *fn, int k, int p)
-{
-	gzFile fp;
+typedef struct {
+	int k, block_len;
 	kseq_t *ks;
-	kc_c2x_t *h;
-	if ((fp = gzopen(fn, "r")) == 0) return 0;
-	ks = kseq_init(fp);
-	h = c2x_init(p);
-	while (kseq_read(ks) >= 0)
-		count_seq(h, k, ks->seq.l, ks->seq.s);
-	kseq_destroy(ks);
-	gzclose(fp);
-	return h;
+	kc_c3x_t *h;
+} pldat_t;
+
+typedef struct {
+	int n, m, sum_len;
+	int *len;
+	char **seq;
+} stepdat_t;
+
+static void *worker_pipeline(void *data, int step, void *in)
+{
+	pldat_t *p = (pldat_t*)data;
+	if (step == 0) {
+		int ret;
+		stepdat_t *s;
+		CALLOC(s, 1);
+		while ((ret = kseq_read(p->ks)) >= 0) {
+			int l = p->ks->seq.l;
+			if (s->n == s->m) {
+				s->m = s->m < 16? 16 : s->m + (s->n>>1);
+				REALLOC(s->len, s->m);
+				REALLOC(s->seq, s->m);
+			}
+			CALLOC(s->seq[s->n], l);
+			memcpy(s->seq[s->n], p->ks->seq.s, l);
+			s->len[s->n++] = l;
+			s->sum_len += l;
+			if (s->sum_len >= p->block_len)
+				break;
+		}
+		if (s->sum_len == 0) free(s);
+		else return s;
+	} else if (step == 1) {
+		stepdat_t *s = (stepdat_t*)in;
+		int i;
+		for (i = 0; i < s->n; ++i) {
+			count_seq(p->h, p->k, s->len[i], s->seq[i]);
+			free(s->seq[i]);
+		}
+		free(s->seq); free(s->len); free(s);
+	}
+	return 0;
 }
 
-static void print_hist(const kc_c2x_t *h)
+static kc_c3x_t *count_file(const char *fn, int k, int p, int block_size)
+{
+	pldat_t pl;
+	gzFile fp;
+	if ((fp = gzopen(fn, "r")) == 0) return 0;
+	pl.ks = kseq_init(fp);
+	pl.k = k;
+	pl.h = c3x_init(p);
+	pl.block_len = block_size;
+	kt_pipeline(2, worker_pipeline, &pl, 2);
+	kseq_destroy(pl.ks);
+	gzclose(fp);
+	return pl.h;
+}
+
+static void print_hist(const kc_c3x_t *h)
 {
 	khint_t k;
 	uint64_t cnt[256];
 	int i;
 	for (i = 0; i < 256; ++i) cnt[i] = 0;
 	for (i = 0; i < 1<<h->p; ++i) {
-		kc_c2_t *g = h->h[i];
+		kc_c3_t *g = h->h[i];
 		for (k = 0; k < kh_end(g); ++k)
 			if (kh_exist(g, k))
 				++cnt[kh_key(g, k)&0xff];
@@ -120,24 +169,26 @@ static void print_hist(const kc_c2x_t *h)
 
 int main(int argc, char *argv[])
 {
-	kc_c2x_t *h;
-	int i, c, k = 31, p = 8;
+	kc_c3x_t *h;
+	int i, c, k = 31, p = 8, block_size = 10000000;
 	ketopt_t o = KETOPT_INIT;
-	while ((c = ketopt(&o, argc, argv, 1, "k:p:", 0)) >= 0)
+	while ((c = ketopt(&o, argc, argv, 1, "k:p:b:", 0)) >= 0) {
 		if (c == 'k') k = atoi(o.arg);
 		else if (c == 'p') p = atoi(o.arg);
+		else if (c == 'b') block_size = atoi(o.arg);
+	}
 	if (argc - o.ind < 1) {
-		fprintf(stderr, "Usage: kc-c2 [-k %d] [-p %d] <in.fa>\n", k, p);
+		fprintf(stderr, "Usage: kc-c2 [-k %d] [-p %d] [-b %d] <in.fa>\n", k, p, block_size);
 		return 1;
 	}
 	if (p < 8) {
 		fprintf(stderr, "ERROR: -p should be at least 8\n");
 		return 1;
 	}
-	h = count_file(argv[o.ind], k, p);
+	h = count_file(argv[o.ind], k, p, block_size);
 	print_hist(h);
 	for (i = 0; i < 1<<p; ++i)
-		kc_c2_destroy(h->h[i]);
+		kc_c3_destroy(h->h[i]);
 	free(h);
 	return 0;
 }
